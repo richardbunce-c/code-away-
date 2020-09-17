83b96a6009dfa65b18f97a1d3815775069f60e1e
Mike Morel <mike@techelevator.com>
2020-09-17 14:08:20 -0400

    M1D3 Lecture

---
+++ module-1/04_Loops_Arrays/lecture-final/Lecture/09_ReturnSumEveryOtherNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    public partial class LectureProblem
+    {
+
+        /*
+        9. We want this loop to only count every other item starting at zero. What needs to change in the loop for it to do that?
+        TOPIC: Looping Through Arrays
+
+            // { 4, 3, 4, 1, 4, 6 }; -> 12
+
+        sum: 12;
+        i: 6;
+
+        */
+        public int ReturnSumEveryOtherNumber(int[] arrayToLoopThrough)
+        {
+
+            //int sum = 0;
+            //for (int i = 0; i < arrayToLoopThrough.Length; i++)
+            //{
+            //    if (i % 2 == 0)  // This means i is even
+            //    {
+            //        sum += arrayToLoopThrough[i];
+            //    }
+            //}
+            //return sum;
+
+            int sum = 0;
+            for (int i = 0; i < arrayToLoopThrough.Length; i += 2)
+            {
+                sum += arrayToLoopThrough[i];
+            }
+            return sum;
+        }
+    }
+}
