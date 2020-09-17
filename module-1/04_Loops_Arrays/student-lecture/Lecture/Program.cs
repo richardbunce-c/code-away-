17a0daa2203231d6290a44be7cbc43ce63e234b7
Mike Morel <mike@techelevator.com>
2020-09-17 07:06:29 -0400

    M1D4 student

---
+++ module-1/04_Loops_Arrays/student-lecture/Lecture/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Use a for-loop to print "Hello World" 10 times
+
+
+
+            // 2. Create an array of quiz scores
+
+            // 3. Print all the scores to the screen
+
+            // 4. Grade on a curve...increase all scores by the curve amount
+
+            // 5. Calculate and print the average score for the class after the curve.
+
+
+        }
+    }
+}
