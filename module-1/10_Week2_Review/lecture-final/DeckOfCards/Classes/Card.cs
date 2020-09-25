3f336dfb4e2aef786d95e6f51f9ca66eb7e8d5b9
Mike Morel <mike@techelevator.com>
2020-09-25 13:18:23 -0400

    M1D10 lecture

---
+++ module-1/10_Week2_Review/lecture-final/DeckOfCards/Classes/Card.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeckOfCards.Classes
+{
+    public class Card
+    {
+        private Dictionary<int, string> cardNames = new Dictionary<int, string>()
+        {
+            {1, "Ace" },
+            {2, "Two" },
+            {3, "Three" },
+            {4, "Four" },
+            {5, "Five" },
+            {6, "Six" },
+            {7, "Seven" },
+            {8, "Eight" },
+            {9, "Nine" },
+            {10, "Ten" },
+            {11, "Jack" },
+            {12, "Queen" },
+            {13, "King" },
+        };
+
+        private Dictionary<string, string> suitNames = new Dictionary<string, string>()
+        {
+            {"H", "Hearts" },
+            {"D", "Diamonds" },
+            {"S", "Spades" },
+            {"C", "Clubs" },
+        };
+
+        // Properties of the Card class
+        public string Suit { get;  }
+
+        // Derived from Suit
+        public string SuitName
+        {
+            get
+            {
+                return suitNames[Suit];
+            }
+        }
+
+        public int Value { get; }
+
+        // "Ace", "Two", "Jack", etc
+        public string ValueName
+        {
+            get
+            {
+                return cardNames[Value];
+            }
+        }
+
+        public string CardTitle
+        {
+            get
+            {
+                return $"{ValueName} of {SuitName}";
+            }
+        }
+
+        // Color is derived from suit
+        public string Color
+        {
+            get
+            {
+                if (Suit == "H" || Suit == "D")
+                {
+                    return "Red";
+                }
+                else
+                {
+                    return "Black";
+                }
+            }
+        }
+
+        public bool IsFaceUp { get; set; } = false;
+
+        // Constructor
+        public Card(string suit, int value)
+        {
+            // TODO: Check that the suit passed ni is valid (S, C, H, D) and value = from 1-13
+
+            Suit = suit;
+            Value = value;
+        }
+
+
+        // Methods
+
+        public void Flip()
+        {
+            //if (IsFaceUp)
+            //{
+            //    IsFaceUp = false;
+            //}
+            //else
+            //{
+            //    IsFaceUp = true;
+            //}
+            IsFaceUp = !IsFaceUp;
+        }
+
+    }
+}
---
+++ module-1/10_Week2_Review/lecture-final/DeckOfCards/Classes/Deck.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeckOfCards.Classes
+{
+    public class Deck
+    {
+        private List<Card> cards = new List<Card>();
+
+        // Properties
+        public int Count
+        {
+            get
+            {
+                return this.cards.Count;
+            }
+        }
+
+        public Deck()
+        {
+            // Create the cards for the deck
+            string[] suits = new string[] { "H", "D", "C", "S" };
+
+            // Loop for every suit
+            foreach (string suit in suits)
+            {
+                // Add a card for every value frmo 1 to 13
+                for( int value = 1; value <= 13; value++)
+                {
+                    Card card = new Card(suit, value);
+                    cards.Add(card);
+                }
+            }
+        }
+
+        // Methods
+        public Card Deal()
+        {
+            if (cards.Count > 0)
+            {
+                Card card = this.cards[0];
+                cards.RemoveAt(0);
+                return card;
+            }
+
+            return null;
+        }
+
+
+    }
+}
