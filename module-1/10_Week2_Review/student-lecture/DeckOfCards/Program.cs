c623e14834253737d1c1697e772b07b82cadf9ba
Mike Morel <mike@techelevator.com>
2020-09-24 20:48:47 -0400

    M1D10 student

---
+++ module-1/10_Week2_Review/student-lecture/DeckOfCards/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Default output encoding (character set) is ASCII
+            // Set it to Unicode so we can display card symbols
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // TODO: Create a card game and start playing!
+
+        }
+
+        
+    }
+}
