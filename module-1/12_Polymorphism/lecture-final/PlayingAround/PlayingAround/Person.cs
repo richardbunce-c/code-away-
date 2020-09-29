41d58c2f65052b4b641f7d9eac57ae5619bd727c
Mike Morel <mike@techelevator.com>
2020-09-29 13:01:50 -0400

    M1D12 lecture

---
+++ module-1/12_Polymorphism/lecture-final/PlayingAround/PlayingAround/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayingAround
+{
+    class Person
+    {
+        public static int staticField = 50;
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, staticField: {staticField}";
+        }
+    }
+}
---
+++ module-1/12_Polymorphism/lecture-final/PlayingAround/PlayingAround/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PlayingAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //decimal cost = 499.52M;
+            //DateTime now = DateTime.Now;
+
+            //Console.WriteLine("The cost is " + cost.ToString("C2"));
+            //Console.WriteLine($"The cost is {cost:C4}");
+
+            //Console.WriteLine($"The date and time is: {now:dddd d}");
+
+
+            Person p1 = new Person() { Name = "Taylor" };
+            Person p2 = new Person() { Name = "Keith" };
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+            p1.Name = "Dave";
+            Person.staticField = 100;
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+        }
+    }
+}
