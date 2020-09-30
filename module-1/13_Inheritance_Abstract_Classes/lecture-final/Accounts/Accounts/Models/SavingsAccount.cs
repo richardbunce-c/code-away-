7db6301b0e3f4bd25c22be6414341cb6f45a85f9
Mike Morel <mike@techelevator.com>
2020-09-30 14:20:49 -0400

    M1D13 lecture

---
+++ module-1/13_Inheritance_Abstract_Classes/lecture-final/Accounts/Accounts/Models/SavingsAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accounts.Models
+{
+    public class SavingsAccount : BankAccount
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount(string acctNum, decimal bal) : base(acctNum, bal)
+        {
+            InterestRate = 0.0025;
+        }
+
+        public override decimal Withdraw(decimal amount)
+        {
+            if (amount > Balance)
+            {
+                // do not allow overdraw
+                return 0;
+            }
+
+            return base.Withdraw(amount);
+        }
+    }
+}
