04a171a7819a87a5360ea86c702c7c71edef808f
Mike Morel <mike@techelevator.com>
2020-10-06 08:21:34 -0400

    M1D17 student

---
+++ module-1/17_FileIO_Writing_out/student-lecture/FileIO_lecture/Tasks/Models/TaskList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Tasks.Models
+{
+    public class TaskList
+    {
+        public string Path { get; set; }
+        private List<Task> taskList { get; set; }
+        public int Count
+        {
+            get
+            {
+                return this.taskList.Count;
+            }
+        }
+        public Task[] List
+        {
+            get
+            {
+                return this.taskList.ToArray();
+            }
+        }
+
+        public Task[] OpenTasks
+        {
+            get
+            {
+                List<Task> openTasks = new List<Task>();
+                foreach (Task t in taskList)
+                {
+                    if (!t.Completed) openTasks.Add(t);
+                }
+                return openTasks.ToArray();
+            }
+        }
+
+        public TaskList(string path)
+        {
+            this.Path = path;
+        }
+
+        public void Load()
+        {
+            this.taskList = new List<Task>();
+
+            // TODO 01: Load tasks from the file (in Path), parse and create Tasks, add them to the list
+
+        }
+
+        public void Save()
+        {
+            // TODO 02: Open the file at Path, and write all the tasks there
+        }
+
+
+        public void AddTask(Task newTask)
+        {
+            this.taskList.Add(newTask);
+            this.Save();
+        }
+
+        public void RemoveTask(Task taskToRemove)
+        {
+            if (this.taskList.Contains(taskToRemove))
+            {
+                this.taskList.Remove(taskToRemove);
+                this.Save();
+            }
+        }
+
+    }
+
+}
