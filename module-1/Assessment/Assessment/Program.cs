77e23a85bf63209e6ca1892f3ac695d97171f421
Mike Morel <mike@techelevator.com>
2020-10-12 08:48:40 -0400

    M1 Assessment and M2D1 student

---
+++ module-1/Assessment/Assessment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Create instances of your object here and call methods.
+
+            Console.ReadLine();
+        }
+    }
+}
