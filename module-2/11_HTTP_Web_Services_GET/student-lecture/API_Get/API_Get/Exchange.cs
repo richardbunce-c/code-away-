ab578c855ba57790c076f52bb261ea1f6dd7f57a
Mike Morel <mike@techelevator.com>
2020-10-25 20:05:38 -0400

    M2D11 student

---
+++ module-2/11_HTTP_Web_Services_GET/student-lecture/API_Get/API_Get/Exchange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API_Get
+{
+    public class Exchange
+    {
+        public string Base { get; set; }
+        public DateTime Date { get; set; }
+        public int TimeLastUpdated { get; set; }
+
+        public DateTime LastUpdated
+        {
+            get
+            {
+                // TimeLastUpdated is a Unix time...convert to DateTime
+                return (new DateTime(1970, 1, 1, 0, 0, 0)).AddSeconds(TimeLastUpdated);
+            }
+        }
+        public Dictionary<string, double> Rates{ get; set; }
+    }
+}
---
+++ module-2/11_HTTP_Web_Services_GET/student-lecture/API_Get/API_Get/ExchangeUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API_Get
+{
+    public class ExchangeUI
+    {
+        public void Run()
+        {
+            ExchangeRateAPI api = new ExchangeRateAPI();
+            while (true)
+            {
+                Console.Write("Enter Currency Code: ");
+                string currency = Console.ReadLine();
+                if (currency.Trim().Length == 0)
+                {
+                    break;
+                }
+                Exchange exchange = api.GetExchange(currency);
+                if (exchange.Rates == null)
+                {
+                    Console.WriteLine($"No currency information found for '{currency}'");
+                    continue;
+                }
+                Console.Clear();
+                Console.WriteLine(@$"Base currency: {exchange.Base}:");
+                foreach (var kvp in exchange.Rates)
+                {
+                    Console.WriteLine($"\t{kvp.Key}{kvp.Value,14:n5}");
+                }
+            }
+        }
+
+    }
+}
