98050352b5e0cf3179658eb81b3068c2acbbd372
Mike Morel <mike@techelevator.com>
2020-10-26 16:03:02 -0400

    M2D11 lecture

---
+++ module-2/11_HTTP_Web_Services_GET/lecture-final/API_Get/API_Get/WeatherUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API_Get
+{
+    public class WeatherUI
+    {
+        public void Run()
+        {
+            WeatherAPI api = new WeatherAPI();
+            while (true)
+            {
+                Console.Write("Enter ZIP Code: ");
+                string zip = Console.ReadLine();
+                if (zip.Trim().Length == 0)
+                {
+                    break;
+                }
+                CurrentWeather weather = api.GetCurrentWeather(zip);
+                if (weather.cod != 200)
+                {
+                    Console.WriteLine($"No weather information found for '{zip}'");
+                    continue;
+                }
+                Console.Clear();
+                Console.WriteLine(@$"
+Current weather in {weather.name} (ZIP: {zip}): 
+    Temperature: {weather.main.temp}F ({weather.main.temp_max}/{weather.main.temp_min})
+    Humidity: {weather.main.humidity}%
+    Feels like: {weather.main.feels_like}F
+"
+                    );
+            }
+        }
+
+    }
+}
