1ba86164666eb878709a1b80cdbd91f14aeeb77f
Mike Morel <mike@techelevator.com>
2020-10-27 08:04:44 -0400

    M2D12 student

---
+++ module-2/12_HTTP_Web_Services_POST/student-lecture/HotelApp/DAL/ReservationApiDao.cs
@@ -0,0 +1,74 @@
+﻿using HTTP_Web_Services_POST_PUT_DELETE_lecture.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace HTTP_Web_Services_POST_PUT_DELETE_lecture.DAL
+{
+    class ReservationApiDao : IReservationDao
+    {
+        private RestClient client;
+
+        public ReservationApiDao(string api_url)
+        {
+            client = new RestClient(api_url);
+        }
+
+        public List<Reservation> GetReservations(int hotelId = 0)
+        {
+            string url;
+            if (hotelId != 0)
+                url = $"hotels/{hotelId}/reservations";
+            else
+                url = "reservations";
+
+            RestRequest request = new RestRequest(url);
+            IRestResponse<List<Reservation>> response = client.Get<List<Reservation>>(request);
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                throw new Exception("Error occurred - unable to reach server.");
+            }
+            else if (!response.IsSuccessful)
+            {
+                throw new Exception("Error occurred - received non-success response: " + (int)response.StatusCode);
+            }
+            else
+            {
+                return response.Data;
+            }
+        }
+
+        public Reservation GetReservation(int reservationId)
+        {
+            RestRequest request = new RestRequest($"reservations/{reservationId}");
+            IRestResponse<Reservation> response = client.Get<Reservation>(request);
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                throw new Exception("Error occurred - unable to reach server.");
+            }
+            else if (!response.IsSuccessful)
+            {
+                throw new Exception("Error occurred - received non-success response: " + (int)response.StatusCode);
+            }
+            else
+            {
+                return response.Data;
+            }
+        }
+
+        public Reservation AddReservation(Reservation newReservation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation UpdateReservation(Reservation reservationToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteReservation(int reservationId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
