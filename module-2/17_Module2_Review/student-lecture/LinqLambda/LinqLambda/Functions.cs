812ba925e25a05e8e418a4fc562b7d978b262699
Mike Morel <mike@techelevator.com>
2020-11-03 10:59:02 -0500

    M2D17 student

---
+++ module-2/17_Module2_Review/student-lecture/LinqLambda/LinqLambda/Functions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqLambda
+{
+    public class Functions
+    {
+        public void ExecuteFunctions()
+        {
+
+
+        }
+
+        IEnumerable<int> forEach(IEnumerable<int> arrayIn, Func<int, int> functionToApply)
+        {
+            List<int> arrayOut = new List<int>();
+            foreach (int element in arrayIn)
+            {
+                int result = functionToApply(element);
+                arrayOut.Add(result);
+            }
+            return arrayOut;
+        }
+
+    }
+}
