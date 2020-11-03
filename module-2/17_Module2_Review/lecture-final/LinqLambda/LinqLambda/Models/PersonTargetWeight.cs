8643b6ca888099c8fb08cec876e846c8068583ec
Mike Morel <mike@techelevator.com>
2020-11-03 17:14:35 -0500

    M2D17 lecture

---
+++ module-2/17_Module2_Review/lecture-final/LinqLambda/LinqLambda/Models/PersonTargetWeight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqLambda.Models
+{
+    public class PersonTargetWeight
+    {
+        public string Name { get; set; }
+        public int TargetWeightLow { get; set; }
+        public int TargetWeightHigh { get; set; }
+
+        public PersonTargetWeight(string name, int low, int high)
+        {
+            Name = name;
+            TargetWeightLow = low;
+            TargetWeightHigh = high;
+        }
+        public override string ToString()
+        {
+            return $"Target weight for {Name} is {TargetWeightLow} - {TargetWeightHigh} pounds.";
+        }
+    }
+}
