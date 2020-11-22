f7465ef0f8c8e98e1d1f262b2c7318cbd8b83152
Richard Bunce <fixtion21@yahoo.com>
2020-11-22 10:19:21 -0500

    finished homework, but npm tests won't run

--- module-3/23_Event_Handling/student-exercise/shopping-list/js/shopping-list.js
+++ module-3/23_Event_Handling/student-exercise/shopping-list/js/shopping-list.js
@@ -12,8 +12,12 @@ const groceries = [
   { id: 9, name: 'Salad', completed: false },
   { id: 10, name: 'Tea', completed: false }
 ];
+
 let listItems;
-let ToggleAllBtn;
+let toggleAllBtn;
+
+const COMPLETED = 'completed';
+
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
@@ -37,57 +41,78 @@ function displayGroceries() {
     ul.appendChild(li);
   });
 }
-document.addEventListener('DOMContentLoaded', ()=>{
+
+document.addEventListener('DOMContentLoaded', () => {
+      
   setPageTitle();
   displayGroceries();
-initListItemListeners();
-markItemComplete();
-markItemIncomplete();
+
+  initListItemListeners();
+
   initToggleAllBtn();
-  toggleAll();
+
 })
 
-function markItemComplete(item){
-if (!item.classList.contains('completed')){
-  item.classList.add('completed');
-}
-}
-
-function markItemIncomplete(item){
-  if (item.classList.contains('completed')){
-    item.classList.remove('completed');
+function markItemComplete(item) {
+  if (!item.classList.contains(COMPLETED)) {
+    item.classList.add(COMPLETED);
   }
 }
 
-function initListItemListeners(){
-   listItems=document.querySelectorAll('li');
-  listItems.forEach((listItem)=> {
-    listItem.addEventListener('click', (event)=>{
-markItemComplete(event.currentTarget);
+function markItemIncomplete(item) {
+  if (item.classList.contains(COMPLETED)) {
+    item.classList.remove(COMPLETED);
+  }
+}
+
+function initListItemListeners() {
+  listItems = document.querySelectorAll('li');
+  listItems.forEach( (listItem) => {
+    listItem.addEventListener('click', (event) => {
+      markItemComplete(event.currentTarget);
     });
 
-    listItem.addEventListener('dblclick', (event)=>{
+    listItem.addEventListener('dblclick', (event) => {
       markItemIncomplete(event.currentTarget);
     });
   });
 }
 
-function toggleAll(){
-if(allItemsIncomplete){
-listItems.forEach(markItemComplete);
-}else{
-listItems.forEach(markItemIncomplete);
+function toggleAll() {
+  if (allItemsIncomplete) {
+    listItems.forEach(markItemComplete);
+  } else {
+    listItems.forEach(markItemIncomplete);
+  }
+
+  toggleAllBtn.innerText = allItemsIncomplete ? 'Mark All Incomplete' : 'Mark All Complete';
+
+  allItemsIncomplete = !allItemsIncomplete;
 }
-toggleAllBtn.innerText = allItemsIncomplete ? 'Mark All Complete' : 'Mark All Incomplete'
 
-
-allItemsIncomplete = !allItemsIncomplete;
-}
-
-function initToggleAllBtn(){
-   toggleAllBtn=document.getElementById('toggleAll');
+function initToggleAllBtn() {
+  toggleAllBtn = document.getElementById('toggleAll');
   toggleAllBtn.addEventListener('click', toggleAll);
 }
 
 
+/********************** FROM REVIEW SECTION **********************/
 
+function testStringInterpolation() {
+  const num = 6;
+  console.log(`Here is our text ${num}`);
+}
+
+function testSubString(myparam = []) {
+  const myString = 'The quick brown fox etc.';
+
+  // substring/slice: startb / stop
+  console.log(myString.substring(4, 6));
+  console.log(myString.slice(4, 6));
+
+  // substr start/number of chars
+  console.log(myString.substr(4, 6));
+
+  // look up string.splice (not necessarily start or end)
+
+}
