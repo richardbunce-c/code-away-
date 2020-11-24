f6cc48c2d99bf66f5b1ca2dfd9f3d7710dff13e0
Richard Bunce <fixtion21@yahoo.com>
2020-11-24 13:12:58 -0500

    finished intro to vue hmwk

--- module-3/30_Introduction_to_Vue_and_Data_Binding/student-exercise/src/components/UserList.vue
+++ module-3/30_Introduction_to_Vue_and_Data_Binding/student-exercise/src/components/UserList.vue
@@ -1,34 +1,54 @@
 <template>
-  <table id="tblUsers">
-    <thead>
-      <tr>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Username</th>
-        <th>Email Address</th>
-        <th>Status</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td><input type="text" id="firstNameFilter" /></td>
-        <td><input type="text" id="lastNameFilter" /></td>
-        <td><input type="text" id="usernameFilter" /></td>
-        <td><input type="text" id="emailFilter" /></td>
-        <td>
-          <select id="statusFilter">
-            <option value="">Show All</option>
-            <option value="Active">Active</option>
-            <option value="Disabled">Disabled</option>
-          </select>
-        </td>
-      </tr>
-      <!-- user listing goes here -->
-      <div class="username" v-for="use in users" v-bind:key="use.username">
+  <div class="container">
+    <table id="tblUsers">
+      <thead>
+        <tr>
+          <th>&nbsp;</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+          <th>Email Address</th>
+          <th>Status</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <input type="text" id="firstNameFilter" v-model="filter.firstName" />
+          </td>
+          <td>
+            <input type="text" id="lastNameFilter" v-model="filter.lastName" />
+          </td>
+          <td>
+            <input type="text" id="usernameFilter" v-model="filter.username" />
+          </td>
+          <td>
+            <input type="text" id="emailFilter" v-model="filter.emailAddress" />
+          </td> 
+          <td>
+            <select id="statusFilter" v-model="filter.status">
+              <option value>Show All</option>
+              <option value="Active">Active</option>
+              <option value="Disabled">Disabled</option> 
+            </select>
+          </td> 
+        </tr>
+        <tr
+          v-for="user in filteredList" :key="user.emailAddress" :class="{disabled: user.status ==='Disabled'}">
+        
+          <td>{{ user.firstName }}</td>
+          <td>{{ user.lastName }}</td>
+          <td>{{ user.username }}</td>
+          <td>{{ user.emailAddress }}</td>
+          <td>{{ user.status }}</td>
+      
+        </tr>
+      </tbody>
+    </table>
 
-      </div>
-    </tbody>
-  </table>
+  
+  </div>
 </template>
 
 <script>
@@ -36,52 +56,113 @@ export default {
   name: "user-list",
   data() {
     return {
+      filter: {
+        firstName: "",
+        lastName: "",
+        username: "",
+        emailAddress: "",
+        status: ""
+      },
+      newUser: {
+        id: null,
+        firstName: "",
+        lastName: "",
+        username: "",
+        emailAddress: "",
+        status: "Active"
+      },
       users: [
         {
+          id: 1,
           firstName: "John",
           lastName: "Smith",
           username: "jsmith",
           emailAddress: "jsmith@gmail.com",
-          status: "Active",
+          status: "Active"
         },
         {
+          id: 2,
           firstName: "Anna",
           lastName: "Bell",
           username: "abell",
           emailAddress: "abell@yahoo.com",
-          status: "Active",
+          status: "Active"
         },
         {
+          id: 3,
           firstName: "George",
           lastName: "Best",
           username: "gbest",
           emailAddress: "gbest@gmail.com",
-          status: "Disabled",
+          status: "Disabled"
         },
         {
+          id: 4,
           firstName: "Ben",
           lastName: "Carter",
           username: "bcarter",
           emailAddress: "bcarter@gmail.com",
-          status: "Active",
+          status: "Active"
         },
         {
+          id: 5,
           firstName: "Katie",
           lastName: "Jackson",
           username: "kjackson",
           emailAddress: "kjackson@yahoo.com",
-          status: "Active",
+          status: "Active"
         },
         {
+          id: 6,
           firstName: "Mark",
           lastName: "Smith",
           username: "msmith",
           emailAddress: "msmith@foo.com",
-          status: "Disabled",
-        },
-      ],
+          status: "Disabled"
+        }
+      ]
     };
   },
+  methods: {},
+  computed: {
+    filteredList() {
+      let filteredUsers = this.users;
+      if (this.filter.firstName != "") {
+        filteredUsers = filteredUsers.filter((user) =>
+          user.firstName
+            .toLowerCase()
+            .includes(this.filter.firstName.toLowerCase())
+        );
+      }
+      if (this.filter.lastName != "") {
+        filteredUsers = filteredUsers.filter((user) =>
+          user.lastName
+            .toLowerCase()
+            .includes(this.filter.lastName.toLowerCase())
+        );
+      }
+      if (this.filter.username != "") {
+        filteredUsers = filteredUsers.filter((user) =>
+          user.username
+            .toLowerCase()
+            .includes(this.filter.username.toLowerCase())
+        );
+      }
+      if (this.filter.emailAddress != "") {
+        filteredUsers = filteredUsers.filter((user) =>
+          user.emailAddress
+            .toLowerCase()
+            .includes(this.filter.emailAddress.toLowerCase())
+        );
+      }
+      if (this.filter.status != "") {
+        filteredUsers = filteredUsers.filter((user) =>
+          user.status === this.filter.status
+        );
+      }
+      return filteredUsers;
+    }
+  }
 };
 </script>
 
@@ -90,6 +171,7 @@ table {
   margin-top: 20px;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  margin-bottom: 20px;
 }
 th {
   text-transform: uppercase;
@@ -104,4 +186,17 @@ input,
 select {
   font-size: 16px;
 }
+
+form {
+  margin: 20px;
+  width: 350px;
+}
+.field {
+  padding: 10px 0px;
+}
+label {
+  width: 140px;
+  display: inline-block;
+}
+
 </style>
--- module-3/31_Vue_Event_Handling/student-lecture/vue-product-reviews/src/components/ProductReview.vue
+++ module-3/31_Vue_Event_Handling/student-lecture/vue-product-reviews/src/components/ProductReview.vue
@@ -1,9 +1,27 @@
 <!--
 Features to add:
   TODO 01: Allow the user to click on a Well to display only those reviews with a particular rating
+        *Create some sort of filter variable
+        *Create a computed filteredReviews list
+        *Change the v-for to loop through the new filteredReviews property
+        *Listen for the click event on a well, and change the filter variable
+
   TODO 02: Give a visual indication of which filter is being applied
+      *Create a class to apply a special style to the selected well
+      *On the Well element, bind to the class^^ if the filterValue matches this well
+
   TODO 03: Provide a form to allow the user to add a new review
+      *Create an object in data() to hold values for a New review to be added
+      *Create a form to display the new review and accept user input values
+      *When user presses "Add" button, add the new review to the reviews array
+      *When the user presses Cancel, clear the form
+
   TODO 04: Show and hide the AddReview form as appropriate
+      *Create a data() variable to track whether the form should be visible
+      *Show the form only when the variable  ^^ is true
+      *Add a link to show the form
+      *On Add, after the review has been added, hide the form
+      *On Cancel, after clearing the form, hide the form
 -->
 <template>
   <div class="main">
@@ -23,14 +41,14 @@ Features to add:
     <!-- TODO 01D: Set the filter when the user clicks on the display -->
     <!-- TODO 02B: Mark which rating is selected -->
     <div class="well-display">
-      <div class="well">
+      <div class="well" v-on:click="changeFilter(0)" v-bind:class="{well: true, selectedFIlter: filterValue === 0}">
         <span class="amount">
           {{ averageRating }}
         </span>
         Average Rating
       </div>
 
-      <div class="well" v-for="i in 5" v-bind:key="i">
+      <div class="well" v-for="i in 5" v-bind:key="i" v-on:click="filterValue = (i)" v-bind:class="{selectedFIlter: filterValue === i}">
         <span class="amount">
           {{nStarReviews(i)}}
         </span>
@@ -41,11 +59,35 @@ Features to add:
     <!-- TODO 04C: Add a link to show or hide the form -->
 
     <!-- TODO 03B: Create the form that allows the user to add a new review -->
+    <a id="showHide" href="#" v-on:click.prevent="showForm =true">Add a new review</a>
+<form v-on:submit.prevent="addReview" v-show="showForm">
+  <div class="error">{{newReviewError}}</div>
+  <!-- Reviewer -->
+  <div class= "form-element">
+    <label for="reviewer">Reviewer Name:</label>
+    <input type="text" id="reviewer" v-model="newReview.reviewer" />
+  </div>
+    <div class= "form-element">
+    <label for="title">Title:</label>
+    <input type="text" id="title" v-model="newReview.title" />
+  </div>
+    <div class= "form-element">
+    <label for="number">Rating:</label>
+    <input type="text" id="rating" v-model.number="newReview.rating" min="1" max="5" />
+  </div>
+    <div class= "form-element">
+    <label for="review">Review:</label>
+    <textarea id="review" v-model="newReview.review" />
+  </div>
+  <input type="submit" value="Save Review" :disabled="newReviewError.length > 0"/>
+  <input type="button" value="Cancel" @click="resetReview" />
+</form>
+
     <!-- TODO 04B: Only show the form of the showForm variable is set -->
 
     <!-- Display each review in a loop -->
     <!-- TODO 01C: Display the filteredReviews array instead of the raw data -->
-    <div class="review" v-for="rev in reviews" v-bind:key="rev.id" v-bind:class="{favorite: rev.favorite}">
+    <div class="review" v-for="rev in filteredReviews" v-bind:key="rev.id" v-bind:class="{favorite: rev.favorite}">
       <h4>{{ rev.reviewer }}</h4>
       <div class="rating">
         <img
@@ -77,10 +119,12 @@ export default {
       description: "Host a party for all your really squirrelly friends!",
 
       // TODO 01A: Create a variable to hold the current ratings Filter value
+      filterValue: 0,
 
       // TODO 03A: Create a new, empty review object for adding new reviews.
-
+      newReview: {},
       // TODO 04A: Create a variable to store whether the Add Review form should be visible
+      showForm: false,
 
       reviews: [
         {
@@ -132,6 +176,30 @@ export default {
           review: "The writer needs to read a 'writing for dummies' book.",
           rating: 1,
           favorite: false
+        },
+            {
+          id: 7,
+          reviewer: "Keith",
+          title: "Yum!",
+          review: "Great recipes in this one.",
+          rating: 4,
+          favorite: false
+        },
+               {
+          id: 8,
+          reviewer: "William",
+          title: "Not so much",
+          review: "This book could have been better.",
+          rating: 1,
+          favorite: false
+        },
+                     {
+          id: 9,
+          reviewer: "Ricky Bobby",
+          title: "If you're not first your last",
+          review: "This book pissed excellence.",
+          rating: 5,
+          favorite: true
         }
       ],
 
@@ -153,16 +221,46 @@ export default {
       return (sumRatings / this.reviews.length).toFixed(2);
     },
     // TODO 01B: Add a computed property filteredReviews to return the reviews to be displayed
-
+    filteredReviews(){
+      //This function filters the "raw" array and returns the subset/filtered array
+      return this.reviews.filter( (r) => {
+        return this. filtervalue === 0 || r.rating === this.filterValue;
+      });
+    },
+    newReviewError(){
+      if ( this.newReview.reviewer && this.reviews.review && this.reviews.rating && this.reviews.rating && this.review.title){
+        return '';
+      } else {
+        return 'Please make sure all fields are filled in';
+      }
+    }
   },
   methods: {
     nStarReviews(n) {
         return this.reviews.filter( r =>{
             return r.rating === n;
         }).length;
-    }
+    },
+    changeFilter(newFilterValue){
+      this.filterValue = newFilterValue;
+    },
     // TODO 03C: Create methods to add the new review or cancel the add
+addReview(){
+    //Validate the newReview has good data
+    if (!this.newReviewError{
 
+    
+    //Add newReview to the array of reviews
+    this.reviews.unshift(this.newReview);
+    this.resetReview();
+    }
+      //Tell the user there's a problem
+    
+},
+resetReview(){
+  this.newReview={}
+  this.showForm
+}
   }
 };
 </script>
@@ -184,12 +282,17 @@ div.main div.well-display div.well {
   border-radius: 6px;
   text-align: center;
   margin: 0.25rem;
+  cursor: pointer;
 }
 
 div.main div.well-display div.well span.amount {
   color: darkslategray;
   display: block;
   font-size: 2.5rem;
+}
+
+.selectedFilter {
+  border:3px solid Green;
 }
 
 div.main div.review {
